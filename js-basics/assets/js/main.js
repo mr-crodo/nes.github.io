518c5a9fbc81ec947443afc6e2e71bbbf0d29dbc
Nasib Mammadov <bduti09@gmail.com>
2020-07-30 01:56:28 +0400

    one day

--- js-basics/assets/js/main.js
+++ js-basics/assets/js/main.js
@@ -1,1 +1,12 @@
-['Игорь', 'Дима', 'Александр'];
+const form =  document.getElementById('form');
+const input = document.querySelector('[name="aa"]');
+
+// sdes mi na formu mojem naveshat klik, a mojem dobavit submit
+form.addEventListener('submit', (event) => {
+  // otmenaem standartnoe brauzernoe povedenie
+  event.preventDefault();
+  // eto dla toqo ctobi v console vvodit to cto mi vveli v inpute
+  console.log(input.value);
+  // a eto mi perezaqrujaem input tem samim ocishaya eqo
+  form.reset();
+})
