6b6b746acfe54cf62131c9957ead8e10d8d7aaf7
Nasib Mammadov <bduti09@gmail.com>
2020-08-26 16:42:29 +0400

    JS day 4

---
+++ java-script-4day/assets/js/dist/main.dev.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+// $(document).ready(function () {
+//   const nMenuBtn = $(".m-menu-button");
+//   const nMenu = $(".m-menu");
+//   nMenuBtn.on('click', function () {
+//     nMenu.toggleClass('active');
+//     $('body').toggleClass('no-scroll');
+//   });
+// });
+// $('.one-time').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+// console.log(obj);
+// alert ('Hello');
+//const result = confirm('Are yo here?'); // Задает вопрос и выходит два выбора ответа
+// console.log(result);
+var ansver = []; // Задает вопрос и выходит место для вставки или написания своего ответа
+// ansver[0] = prompt('Как ваше имя?', '');
+// ansver[1] = prompt('Как ваше фамилия?', '');
+// ansver[2] = prompt('Сколько вам лет?', '');
+// document.write(ansver);
+
+console.log(_typeof(ansver));
+console.log(_typeof(null));
---
+++ java-script-4day/assets/js/main.js
@@ -0,0 +1,39 @@
+// $(document).ready(function () {
+//   const nMenuBtn = $(".m-menu-button");
+//   const nMenu = $(".m-menu");
+//   nMenuBtn.on('click', function () {
+//     nMenu.toggleClass('active');
+//     $('body').toggleClass('no-scroll');
+//   });
+// });
+
+// $('.one-time').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+//   adaptiveHeight: true
+// });
+
+// const obj = {
+//   name: "John",
+//   age: 25,
+//   isMarried: false,
+// };
+
+// console.log(obj);
+
+// alert ('Hello');
+
+//const result = confirm('Are yo here?'); // Задает вопрос и выходит два выбора ответа
+// console.log(result);
+
+const ansver = []; // Задает вопрос и выходит место для вставки или написания своего ответа
+
+// ansver[0] = prompt('Как ваше имя?', '');
+// ansver[1] = prompt('Как ваше фамилия?', '');
+// ansver[2] = prompt('Сколько вам лет?', '');
+
+// document.write(ansver);
+console.log(typeof(ansver));
+console.log(typeof
